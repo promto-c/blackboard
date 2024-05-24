44ae22a33235b3021ebd59ca3961defab2beb6c9
promto-c <prommm01@gmail.com>
2024-05-24 14:17:23 +0700

    add menu

--- blackboard/examples/example_file_browser.py
+++ blackboard/examples/example_file_browser.py
@@ -1,4 +1,4 @@
-from typing import Tuple, Optional, List, Dict
+from typing import Tuple, Optional, List, Dict, Union
 
 import subprocess
 import re
@@ -7,6 +7,13 @@ import sys
 import configparser
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+
+from enum import Enum
+
+class ApplicationSection(Enum):
+    DEFAULT = 'default'
+    REGISTERED = 'registered'
+    RECOMMENDED = 'recommended'
 
 class ApplicationUtils:
     
@@ -53,16 +60,22 @@ class ApplicationUtils:
         return data
 
     @staticmethod
-    def get_associated_apps(mime_type: str, section: str = 'registered') -> List[str]:
+    def get_associated_apps(mime_type: str, section: Union[ApplicationSection, str] = ApplicationSection.DEFAULT) -> List[str]:
         """List applications associated with a given MIME type.
         
         Args:
             mime_type (str): The MIME type to search for associated applications.
-            section (str): The section of applications to return ('default', 'registered', 'recommended').
+            section (Union[ApplicationSection, str]): The section of applications to return ('default', 'registered', 'recommended').
             
         Returns:
             List[str]: A list of paths to the .desktop files of the specified section.
         """
+        if isinstance(section, str):
+            try:
+                section = ApplicationSection(section.lower())
+            except ValueError:
+                raise ValueError(f"Invalid section '{section}'. Choose from 'default', 'registered', 'recommended'.")
+        
         result = subprocess.check_output(['gio', 'mime', mime_type]).decode().strip()
         parsed_data = ApplicationUtils.parse_gio_mime_output(result)
 
@@ -77,10 +90,10 @@ class ApplicationUtils:
             ApplicationUtils.find_desktop_file(app) for app in parsed_data['recommended'] if ApplicationUtils.find_desktop_file(app) is not None
         ]
 
-        if section not in parsed_data:
-            raise ValueError(f"Invalid section '{section}'. Choose from 'default', 'registered', 'recommended'.")
+        if section.value not in parsed_data:
+            raise ValueError(f"Invalid section '{section.value}'. Choose from 'default', 'registered', 'recommended'.")
 
-        return parsed_data[section]
+        return parsed_data[section.value]
 
     @staticmethod
     def parse_desktop_file(desktop_file: str) -> Tuple[Optional[str], Optional[str]]:
@@ -151,7 +164,7 @@ class AppSelectionDialog(QtWidgets.QDialog):
     def get_applications_for_mime_type(self, file_path: str) -> List[str]:
         try:
             mime_type = ApplicationUtils.get_mime_type(file_path)
-            applications = ApplicationUtils.get_associated_apps(mime_type)
+            applications = ApplicationUtils.get_associated_apps(mime_type, section=ApplicationSection.REGISTERED)
             return applications
         except subprocess.CalledProcessError:
             return []
@@ -167,21 +180,41 @@ class CustomTreeView(QtWidgets.QTreeView):
     def open_menu(self, position):
         indexes = self.selectedIndexes()
         if len(indexes) > 0:
-            level = 0
             index = indexes[0]
-            while index.parent().isValid():
-                index = index.parent()
-                level += 1
-
             menu = QtWidgets.QMenu()
-            if QtCore.QFileInfo(self.model().filePath(indexes[0])).isFile():
+            
+            if QtCore.QFileInfo(self.model().filePath(index)).isFile():
                 open_action = QtWidgets.QAction('Open', self)
                 open_with_action = QtWidgets.QAction('Open with...', self)
+                open_containing_folder_action = QtWidgets.QAction('Open Containing Folder', self)
+                open_in_terminal_action = QtWidgets.QAction('Open in Terminal', self)
+                copy_file_path_action = QtWidgets.QAction('Copy Path', self)
+                copy_relative_path_action = QtWidgets.QAction('Copy Relative Path', self)
+
+                open_action.triggered.connect(lambda: self.open_file(index))
+                open_with_action.triggered.connect(lambda: self.open_file_with(indexes[0]))
+                open_containing_folder_action.triggered.connect(lambda: self.open_containing_folder(index))
+                open_in_terminal_action.triggered.connect(lambda: self.open_in_terminal(index))
+                copy_file_path_action.triggered.connect(lambda: self.copy_file_path(index))
+                copy_relative_path_action.triggered.connect(lambda: self.copy_relative_path(index))
+
                 menu.addAction(open_action)
                 menu.addAction(open_with_action)
-                open_action.triggered.connect(lambda: self.open_file(indexes[0]))
-                open_with_action.triggered.connect(lambda: self.open_file_with(indexes[0]))
-                menu.exec_(self.viewport().mapToGlobal(position))
+                menu.addAction(open_containing_folder_action)
+                menu.addAction(open_in_terminal_action)
+                menu.addAction(copy_file_path_action)
+                menu.addAction(copy_relative_path_action)
+            else:
+                new_file_action = QtWidgets.QAction('New File', self)
+                new_folder_action = QtWidgets.QAction('New Folder', self)
+                
+                new_file_action.triggered.connect(lambda: self.new_file(index))
+                new_folder_action.triggered.connect(lambda: self.new_folder(index))
+
+                menu.addAction(new_file_action)
+                menu.addAction(new_folder_action)
+
+            menu.exec_(self.viewport().mapToGlobal(position))
 
     def open_file(self, index):
         file_path = self.model().filePath(index)
@@ -195,6 +228,78 @@ class CustomTreeView(QtWidgets.QTreeView):
                 ApplicationUtils.open_file_with_application(file_path, dialog.selected_application)
             except Exception as e:
                 QtWidgets.QMessageBox.warning(self, "Error", f"Failed to launch application: {str(e)}")
+
+    def open_containing_folder(self, index):
+        file_path = self.model().filePath(index)
+        folder_path = os.path.dirname(file_path)
+        subprocess.run(['xdg-open', folder_path])
+
+    def open_in_terminal(self, index):
+        file_path = self.model().filePath(index)
+        folder_path = os.path.dirname(file_path)
+        terminal_command = os.getenv('TERMINAL', 'gnome-terminal')
+        subprocess.run([terminal_command, '--working-directory', folder_path])
+
+    def copy_file_path(self, index):
+        file_path = self.model().filePath(index)
+        clipboard = QtWidgets.QApplication.clipboard()
+        clipboard.setText(file_path)
+
+    def copy_relative_path(self, index):
+        file_path = self.model().filePath(index)
+        relative_path = os.path.relpath(file_path, start=os.path.expanduser("~"))
+        clipboard = QtWidgets.QApplication.clipboard()
+        clipboard.setText(relative_path)
+
+    def rename_file(self, index):
+        file_path = self.model().filePath(index)
+        new_name, ok = QtWidgets.QInputDialog.getText(self, 'Rename File', 'New Name:')
+        if ok:
+            new_path = os.path.join(os.path.dirname(file_path), new_name)
+            os.rename(file_path, new_path)
+            self.model().setRootPath('')
+            self.model().setRootPath('/')
+
+    def delete_file(self, index):
+        file_path = self.model().filePath(index)
+        os.remove(file_path)
+        self.model().remove(index)
+
+    def duplicate_file(self, index):
+        file_path = self.model().filePath(index)
+        new_path = file_path + '_copy'
+        with open(file_path, 'rb') as fsrc, open(new_path, 'wb') as fdst:
+            fdst.write(fsrc.read())
+        self.model().setRootPath('')
+        self.model().setRootPath('/')
+
+    def move_to_trash(self, index):
+        file_path = self.model().filePath(index)
+        trash_path = os.path.join(os.path.expanduser('~/.local/share/Trash/files/'), os.path.basename(file_path))
+        os.rename(file_path, trash_path)
+        self.model().remove(index)
+
+    def show_properties(self, index):
+        file_path = self.model().filePath(index)
+        file_info = QtCore.QFileInfo(file_path)
+        details = f"File: {file_info.fileName()}\nSize: {file_info.size()} bytes\nType: {file_info.suffix()}\nModified: {file_info.lastModified().toString()}"
+        QtWidgets.QMessageBox.information(self, 'Properties', details)
+
+    def new_file(self, index):
+        directory = self.model().filePath(index)
+        new_file_path, ok = QtWidgets.QInputDialog.getText(self, 'New File', 'File Name:')
+        if ok:
+            open(os.path.join(directory, new_file_path), 'a').close()
+            self.model().setRootPath('')
+            self.model().setRootPath('/')
+
+    def new_folder(self, index):
+        directory = self.model().filePath(index)
+        new_folder_path, ok = QtWidgets.QInputDialog.getText(self, 'New Folder', 'Folder Name:')
+        if ok:
+            os.makedirs(os.path.join(directory, new_folder_path))
+            self.model().setRootPath('')
+            self.model().setRootPath('/')
 
     def open_file_on_double_click(self, index):
         if QtCore.QFileInfo(self.model().filePath(index)).isFile():
