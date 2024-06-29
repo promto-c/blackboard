2a55e27de38ac08bd2052ffc86d5152a9a7b69c8
promto-c <prommm01@gmail.com>
2024-06-30 02:39:39 +0700

    add util

--- blackboard/utils/text_utils.py
+++ blackboard/utils/text_utils.py
@@ -1,10 +1,58 @@
-import re
+# Type Checking Imports
+# ---------------------
 from typing import List
 
+# Standard Library Imports
+# ------------------------
+import re
+
+
+# Class Definitions
+# -----------------
+class TextUtil:
+    """Provides static methods for various text-related operations.
+    """
+
+    @staticmethod
+    def find_positions(text: str, substring: str, case_sensitive: bool = True) -> List[int]:
+        """Find all positions of a substring within a text.
+
+        Args:
+            text (str): The text in which to search for positions of the substring.
+            substring (str): The substring to search for within the text.
+            case_sensitive (bool, optional): If False, perform a case-insensitive search (default is True).
+
+        Returns:
+            List[int]: A list of indices where the substring is found in the text.
+
+        Example:
+        >>> text = "This is a test sentence. This test has multiple occurrences. Test is repeated to test the function."
+        >>> TextUtil.find_positions(text, "test")
+        [10, 30, 81]
+        >>> TextUtil.find_positions(text, "test", case_sensitive=False)
+        [10, 30, 61, 81]
+        """
+        indexes = []
+        sub_len = len(substring)
+
+        if not case_sensitive:
+            text = text.lower()
+            substring = substring.lower()
+
+        start_find = 0
+        while True:
+            index = text.find(substring, start_find)
+            if index == -1:
+                break
+            indexes.append(index)
+            start_find = index + sub_len
+
+        return indexes
 
 class TextExtraction:
     """Provides static methods for extracting quoted and unquoted terms from strings.
     """
+
     @staticmethod
     def extract_quoted_terms(keyword: str) -> List[str]:
         """Extracts terms enclosed in quotes from the given keyword string.
